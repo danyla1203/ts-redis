9c9c9a2ef15ee29f7b54e6a5f72e01eb08087201
Daniil <zelenko.danil@gmail.com>
2021-01-08 19:44:37 +0200

    del method. if err reject with err obj

--- index.ts
+++ index.ts
@@ -9,31 +9,38 @@ export class ComfortRedis {
     public keys(pattern: string): Promise<string[]> {
         return new Promise((res, rej) => {
             this.conn.keys(pattern, (err, result) => {
-                if (result instanceof Error) { rej(err) }
+                if (err instanceof Error) { rej(err) }
                 else { res(result) }
             })
         })
     }
     public hgetall(key: string): Promise<any> {
         return new Promise((res, rej) => {
-            this.conn.hgetall(key, (err: any, result: any) => {
-                if (result instanceof Error) { rej(err) }
+            this.conn.hgetall(key, (err, result: any) => {
+                if (err instanceof Error) { rej(err) }
                 else { res(result) }
             })
         })
     }
-    public hmset(key: string, data: Object) {
+    public hmset(key: string, data: Object): Promise<string> {
         return new Promise((res, rej) => {
             let arrayedObj = Object.entries(data);
             let mixinArr: any[] = arrayedObj.map((el: [string, any]) => {
                 mixinArr.push(el[0]);
                 mixinArr.push(el[1]);
             })
-            this.conn.hmset(key, ...mixinArr , (err: any, result: any) => {
-                if (result instanceof Error) { rej(err) }
+            this.conn.hmset(key, ...mixinArr , (err: any, result: string) => {
+                if (err instanceof Error) { rej(err) }
+                else { res(result) }
+            })
+        })
+    }
+    public del(key: string): Promise<number> {
+        return new Promise((res, rej) => {
+            this.conn.del(key, (err, result) => {
+                if (err instanceof Error) { rej(err) }
                 else { res(result) }
             })
         })
     }
 }
-
